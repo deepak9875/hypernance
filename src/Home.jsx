cd69bc2788dcc504de3db7d283591653123d0598
deepak9875 <deepaklakhlan1234@gmail.com>
2024-02-12 19:56:12 +0530

    make some change in home file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex gap-3 justify-center mt-10">
+      <Link to={`/signin`}>
+        <button className="w-[200px] h-[48px] rounded-lg bg-black text-white">
+          sign in
+        </button>
+      </Link>
+      <Link to={`/signup`}>
+        <button className="w-[200px] h-[48px] rounded-lg bg-black text-white">
+          sign up
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
